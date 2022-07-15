28cb24ec03c9d26855546067503159c0ab2aa90c
DawilH <dawilherrerareyes@gmail.com>
2022-07-15 14:27:15 -0400

    Add project files.

---
+++ Datos/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Datos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.btninsertar = new System.Windows.Forms.Button();
+            this.btneliminarde = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.txtposicion = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.liver = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(267, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 63);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Base de datos";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(77, 179);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 44);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "AGREGAR";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.AcceptsReturn = true;
+            this.txtvalor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtvalor.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvalor.Location = new System.Drawing.Point(304, 176);
+            this.txtvalor.Multiline = true;
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(208, 44);
+            this.txtvalor.TabIndex = 3;
+            this.txtvalor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnagregar.Location = new System.Drawing.Point(77, 301);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(94, 35);
+            this.btnagregar.TabIndex = 4;
+            this.btnagregar.Text = "AGREGAR";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrar.Location = new System.Drawing.Point(177, 301);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(94, 35);
+            this.btnmostrar.TabIndex = 5;
+            this.btnmostrar.Text = "MOSTRAR";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // btninsertar
+            // 
+            this.btninsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninsertar.Location = new System.Drawing.Point(77, 342);
+            this.btninsertar.Name = "btninsertar";
+            this.btninsertar.Size = new System.Drawing.Size(94, 35);
+            this.btninsertar.TabIndex = 6;
+            this.btninsertar.Text = "INSERTAR";
+            this.btninsertar.UseVisualStyleBackColor = true;
+            this.btninsertar.Click += new System.EventHandler(this.btninsertar_Click);
+            // 
+            // btneliminarde
+            // 
+            this.btneliminarde.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneliminarde.Location = new System.Drawing.Point(77, 383);
+            this.btneliminarde.Name = "btneliminarde";
+            this.btneliminarde.Size = new System.Drawing.Size(94, 35);
+            this.btneliminarde.TabIndex = 7;
+            this.btneliminarde.Text = "ELIMINAR DE";
+            this.btneliminarde.UseVisualStyleBackColor = true;
+            this.btneliminarde.Click += new System.EventHandler(this.btneliminarde_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneliminar.Location = new System.Drawing.Point(77, 424);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(94, 35);
+            this.btneliminar.TabIndex = 8;
+            this.btneliminar.Text = "ELIMINAR";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // txtposicion
+            // 
+            this.txtposicion.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtposicion.Location = new System.Drawing.Point(294, 416);
+            this.txtposicion.Multiline = true;
+            this.txtposicion.Name = "txtposicion";
+            this.txtposicion.Size = new System.Drawing.Size(208, 44);
+            this.txtposicion.TabIndex = 10;
+            this.txtposicion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(314, 378);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(168, 35);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "POSICION";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // liver
+            // 
+            this.liver.AcceptsReturn = true;
+            this.liver.AcceptsTab = true;
+            this.liver.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.liver.AllowDrop = true;
+            this.liver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.liver.HideSelection = false;
+            this.liver.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.liver.Location = new System.Drawing.Point(562, 251);
+            this.liver.Multiline = true;
+            this.liver.Name = "liver";
+            this.liver.ReadOnly = true;
+            this.liver.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.liver.Size = new System.Drawing.Size(223, 228);
+            this.liver.TabIndex = 9;
+            this.liver.Text = "Base de datos";
+            this.liver.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.liver.TextChanged += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(797, 519);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.txtposicion);
+            this.Controls.Add(this.liver);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btneliminarde);
+            this.Controls.Add(this.btninsertar);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "++";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.Button btninsertar;
+        private System.Windows.Forms.Button btneliminarde;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.TextBox txtposicion;
+        private System.Windows.Forms.TextBox textBox6;
+        public System.Windows.Forms.TextBox liver;
+    }
+}
+
---
+++ Datos/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datos
+{
+   
+    public partial class Form1 : Form
+    {
+        List<int> Milista = new List<int>(); 
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            int n;
+            n = int.Parse(txtvalor.Text);
+            Milista.Add(n);
+            txtvalor.Clear();
+            txtvalor.Focus();
+
+        }
+
+        private void btninsertar_Click(object sender, EventArgs e)
+        {
+            int n, pos;
+            n = int.Parse(txtvalor.Text);
+            pos = int.Parse(txtposicion.Text);
+            Milista.Insert(n, pos);
+            txtvalor.Clear();
+            txtvalor.Focus();
+            txtposicion.Clear();
+            txtposicion.Focus();
+        }
+
+        private void btneliminarde_Click(object sender, EventArgs e)
+        {
+            int pos;
+            pos= int.Parse(txtposicion.Text);
+            txtposicion.Clear();
+            txtposicion.Focus();
+            Milista.RemoveAt(pos);
+
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            int n;
+            n = int.Parse(txtvalor.Text);
+            Milista.Remove(n);
+            txtvalor.Clear();
+            txtvalor.Focus();
+
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            int i;
+            liver.Items.Clear();
+            for (i = 0; i <= Milista.Count - 1; i++)
+            {
+                liver.Items.Add(Milista.ElementAt(i));
+            }
+
+        }
+    }
+}
